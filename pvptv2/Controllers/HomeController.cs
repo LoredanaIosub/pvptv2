e6bdc90dd848723065f1a598bd202d7876c83461
Loredana Iosub <loredana.iosub28@gmail.com>
2019-05-07 21:31:37 +0300

    -reparata aplicatia

--- pvptv2/Controllers/HomeController.cs
+++ pvptv2/Controllers/HomeController.cs
@@ -15,7 +15,42 @@ namespace pvptv2.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Descoperă frumusețea Brașovului";
+
+            return View();
+        }
+
+        public ActionResult Travel()
+        {
+            ViewBag.Message = "Bucură-te de experiențe minunate";
+
+            return View();
+        }
+
+        public ActionResult Activities()
+        {
+            ViewBag.Message = "Activități";
+
+            return View();
+        }
+
+        public ActionResult Relax()
+        {
+            ViewBag.Message = "Relaxare";
+
+            return View();
+        }
+
+        public ActionResult Eat()
+        {
+            ViewBag.Message = "Mancare buna.";
+
+            return View();
+        }
+
+        public ActionResult Play()
+        {
+            ViewBag.Message = "Play";
 
             return View();
         }
@@ -23,6 +58,27 @@ namespace pvptv2.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Events()
+        {
+            ViewBag.Message = "Evenimente";
+
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            ViewBag.Message = "Noutati";
+
+            return View();
+        }
+
+        public ActionResult Opinion()
+        {
+            ViewBag.Message = "Parerea ta!";
 
             return View();
         }
---
+++ pvptv2/Controllers/TravelController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pvptv2.Controllers
+{
+    public class TravelController : Controller
+    {
+        // GET: Travel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sinaia()
+        {
+            return View();
+        }
+
+        public ActionResult Predeal()
+        {
+            return View();
+        }
+
+        public ActionResult PoianaBrasov()
+        {
+            return View();
+        }
+
+        public ActionResult CastelulBran()
+        {
+            return View();
+        }
+
+        public ActionResult CetateaRasnov()
+        {
+            return View();
+        }
+
+        public ActionResult SatulViscri()
+        {
+            return View();
+        }
+    }
+}
